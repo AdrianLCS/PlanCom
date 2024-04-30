1046a0765a236bf90daa0d88bbaeb8d21f97809b
will91dsl <will04.ws@gmail.com>
2024-04-30 10:33:52 -0300

    atualização do arquivo radio.py

--- Radios.py
+++ Radios.py
@@ -1,24 +1,97 @@
 class Radio():
-    def __int__(self, sensibilidad, freq, potencia,antena, ganho):
-        self.sensibilidad=sensibilidad
-        self.faxa_de_freq = freq
+    def __int__(self, sensibilidade, freq, potencia,antena, ganho):
+        self.sensibilidade=sensibilidade
+        self.faixa_de_freq = freq
         self.potencia = []
-        self.antenas =antena #{'blade': 1.5, ''}
+        self.antenas {'blade': 1.5, ''}
         self.ganho = ganho
+    def SelFreq(self,freq):
+        return self.
+    def executar(self,*args,**kwargs):
+        if
+        return self.SelFreq(*args,**kwargs)
 
-#rf7800vhh = radio(sensibilidade=-116)
 
-RF7800  = Radio()
-RF7800S = Radio()
-RF7800H = Radio()
-RF7850S = Radio()
-RF7800M = Radio()
-RF7800V = Radio()
-MPR9600 = Radio()
+rf7800vhh = radio(sensibilidade=-116)
+s=rf7800vhh.sensibilidade
+"""""
+if freq<=4.4 and freq>=5:
+    freq = int(input())
+    else
+    return f'Valor de frequência {freq} não disponível para o modelo'
+"""
+RF7800W_OU470 = Radio(sensibilidade=-98, freq='', potencia = 10, antena , ganho )
+RF7800W_OU500 = Radio(sensibilidade = -98, freq='', potencia = 10, antena, ganho)
+RF7800W_OU500.faixa_de_freq = float(input())
+if RF7800W_OU500 >= 4400 and RF7800W_OU500 <= 5000:
+    frequencia = RF7800W_OU500.faixa_de_freq
+else
+    frequencia = 'fora da faixa de operação'
+"""
+Faixa de Freq:
+RF - 7800W-OU470: 4,4 - 5,0 GHz
+RF - 7800W-OU500: 4,4 - 5,875 GHz
+Potencia de transmissão: 18dBm (4,4 - 4,7 GHz); 20dBm(4,7 - 4,9 GHz);
+22 dBm (4,9 - 5,875 GHz)
+Sensibilidade de recepção: -98 dBm
+"""
+RF7800S = Radio(sensibilidade, freq, potencia , antena, ganho)
+"""
+Faixa de Freq:
+350,000 a 450,000MHz
+Potencia de saída: 2W, ajustavel para 1W , 0.25W?
+Sensililidade de Saida de audio: 0 a 75dBa com fone de ouvido?
+antenas:
+GPS 12006-0017-01
+Veicular RF-3184-AT320 225 até 450Mhz
+"""
+RF7800H = Radio(sensibilidade, freq, potencia , antena, ganho) # verificar a faixa de frequencia. a principio está abaixo do esperado
+"""
+faixa de freq:
+1,5MHz e 59,9999MHz com alta potencia de saída de 10 W
+potencia de saída selecionãvel em baixa(1W),média (5 W) e alta (20 W)
 
-APX2000 = Radio()
-APX2500 = Radio()
-APX2000 = Radio()
-APX2000 = Radio()
-APX2000 = Radio()
-APX2000 = Radio()
+"""
+RF7850S = Radio(sensibilidade, freq, potencia , antena, ganho)
+"""
+faixa de freq:
+TGW2 e S-TNW 225 - 2500 MHz
+LOS/FSK 225 - 512 MHz
+TNW 225 - 511.975 MHz
+Poencia saida: 3W
+sensibilidade: -116dBm @12dB SINAD (sinal+ruido+distorção)
+"""
+
+RF7800M-MP = Radio(sensibilidade, freq, potencia , antena, ganho)
+"""
+faixa de freq:
+VHF de banda baixa - 30MHz a 89,9999 MHz
+VHF de banda alta - 90 MHz a 224,9999 MHz
+Banda UHF - 225 MHz a 511,9950 MHz
+Instalação da antena:
+20 W (225 MHz - 1999,9950 MHz) 
+10 W (30MHz - 511,9950MHz)
+antena de GPS
+"""
+
+RF7800V-V51X = Radio(sensibilidade, freq, potencia , antena, ganho)
+"""
+Faixa de freq:
+30 MHz a 108 MHz até 50 W
+"""
+
+MPR9600 = Radio(sensibilidade, freq, potencia , antena, ganho) # retirar abaixo da frequencia de trabalho
+"""
+faixa de freq:
+1.6MHz a 29,9999MHz
+sensibilidade: -113dBm para 10dB SINAD (sinal+ruido+distorção)
+antenas:
+GPS
+WHIP OE-505
+"""
+APX2000 = Radio(sensibilidade, freq, potencia , antena, ganho)
+APX2500 = Radio(sensibilidade, freq, potencia , antena, ganho)
+APX2000 = Radio(sensibilidade, freq, potencia , antena, ganho)
+APX2000 = Radio(sensibilidade, freq, potencia , antena, ganho)
+APX2000 = Radio(sensibilidade, freq, potencia , antena, ganho)
+APX2000 = Radio(sensibilidade, freq, potencia , antena, ganho)
